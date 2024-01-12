724bfcb7e6b9d7d9763750b823a92222d65fd2d2
iyoub <abbous.ayoub@gmail.com>
2024-01-12 22:58:48 +0100

    add light butons

--- src/components/BoxCard.vue
+++ src/components/BoxCard.vue
@@ -1,9 +1,23 @@
 <template>
-  <div class="p-5 flex items-center self-center mx-auto border h-fit w-fit">
+  <div class="p-5 flex items-center self-center mx-auto h-fit w-fit relative">
+    <button class="absolute top-0 left-0">
+      <IconBulb />
+    </button>
+    <button class="absolute top-0 right-0">
+      <IconBulb />
+    </button>
+    <button class="absolute bottom-0 left-0">
+      <IconBulb />
+    </button>
+    <button class="absolute bottom-0 right-0">
+      <IconBulb />
+    </button>
     <div class="card-preview flex items-center justify-center">
       <div class="form-label">Preview</div>
     </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import IconBulb from './IconBulb.vue'
+</script>
