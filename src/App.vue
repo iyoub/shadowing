9c685376899e1ac693cfa6c005086a734efb1a87
iyoub <abbous.ayoub@gmail.com>
2024-01-15 21:41:01 +0100

    unifyinf tpography classes

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <main class="min-h-screen bg-neutral-100/20 flex">
     <div class="container mx-auto px-6 flex flex-col">
       <NavBar />
-      <div class="my-auto py-8">
+
+      <section class="my-10">
         <div class="grid sm:grid-cols-2 grid-cols-1 gap-12 mb-12">
           <BoxCard />
           <ControlsCard />
@@ -10,7 +11,11 @@
         <div class="flex justify-center w-auto sticky bottom-2">
           <HighlightCode :code="code" />
         </div>
-      </div>
+      </section>
+
+      <section class="my-10">
+        <ShadowPresets />
+      </section>
     </div>
   </main>
 </template>
@@ -21,4 +26,5 @@ import ControlsCard from '@/components/ControlsCard.vue'
 import HighlightCode from './components/HighlightCode.vue'
 import NavBar from './components/NavBar.vue'
 import { code } from './states/styles'
+import ShadowPresets from './components/ShadowPresets.vue'
 </script>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="text-center py-4">
-    <h1 class="text-3xl mb-2 font-bold">Shadowing</h1>
-    <p class="text-sm text-neutral-500">A simple tool to generate box-shadow CSS</p>
+    <h1 class="title">Shadowing</h1>
+    <p class="subtitle">A simple tool to generate box-shadow CSS</p>
   </nav>
 </template>
 
