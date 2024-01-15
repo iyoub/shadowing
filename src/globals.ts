0b03693401e8a2f0769129de661756c328fb266c
iyoub <abbous.ayoub@gmail.com>
2024-01-15 22:11:36 +0100

    added global preset shadows

--- src/globals.ts
+++ src/globals.ts
@@ -17,8 +17,48 @@ export const RANGE_VALUES = {
 	},
 };
 
-
-
 export const DEFAULT_COLORS = [
 	'#fcfcfc', '#f2f2f2', '#e6e6e6', '#d9d9d9', '#cccccc', '#bfbfbf', '#b3b3b3', '#a6a6a6', '#999999', '#8c8c8c', '#808080', '#737373', '#666666', '#595959', '#4d4d4d', '#404040', '#333333', '#262626', '#1a1a1a', '#0d0d0d', '#000000',
+];
+
+export const SHADOW_PRESETS = [
+	{
+		shadow: '0px 0px 5px #e3e3e3',
+	},
+	{
+		shadow: '0px 0px 5px #d8d8d8',
+	},
+	{
+		shadow: '0px 0px 5px #cccccc',
+	},
+	{
+		shadow: '0px 0px 5px #c1c1c1',
+	},
+	{
+		shadow: '0px 0px 5px #b6b6b6',
+	},
+	{
+		shadow: '0px 0px 5px #ababab',
+	},
+	{
+		shadow: '0px 0px 5px #a0a0a0',
+	},
+	{
+		shadow: '0px 0px 5px #949494',
+	},
+	{
+		shadow: '0px 0px 5px #898989',
+	},
+	{
+		shadow: '0px 0px 5px #7e7e7e',
+	},
+	{
+		shadow: '0px 0px 5px #737373',
+	},
+	{
+		shadow: '0px 0px 5px #676767',
+	},
+	{
+		shadow: '0px 0px 5px #5c5c5c',
+	}
 ];
