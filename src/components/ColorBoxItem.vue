e98091360b9597f250f343039302bad5dee574c4
iyoub <abbous.ayoub@gmail.com>
2024-01-16 21:29:02 +0100

    added new shadow presets and highliting the selected color

--- src/components/ColorBoxItem.vue
+++ src/components/ColorBoxItem.vue
@@ -1,12 +1,16 @@
 <template>
   <div
-    class="border h-8 w-8 rounded-full cursor-pointer hover:scale-[1.1] hover:shadow transition-all duration-100"
+    class="border ring-0 h-8 w-8 rounded-full cursor-pointer hover:scale-[1.1] transition-all duration-100"
+    :class="{ '!ring-2 ring-slate-500': isColorSelected }"
     :style="{ backgroundColor: props.color }"
     @click="emit('selectColor', props.color)"
-  ></div>
+  />
 </template>
 
 <script setup lang="ts">
+import { styles } from '@/states/styles'
+import { computed } from 'vue'
+
 const props = defineProps({
   color: {
     type: String,
@@ -15,4 +19,6 @@ const props = defineProps({
 })
 
 const emit = defineEmits(['selectColor'])
+
+const isColorSelected = computed(() => styles.value.color === props.color)
 </script>
--- src/globals.ts
+++ src/globals.ts
@@ -20,45 +20,15 @@ export const RANGE_VALUES = {
 export const DEFAULT_COLORS = [
 	'#fcfcfc', '#f2f2f2', '#e6e6e6', '#d9d9d9', '#cccccc', '#bfbfbf', '#b3b3b3', '#a6a6a6', '#999999', '#8c8c8c', '#808080', '#737373', '#666666', '#595959', '#4d4d4d', '#404040', '#333333', '#262626', '#1a1a1a', '#0d0d0d', '#000000',
 ];
-
 export const SHADOW_PRESETS = [
+  {
+    shadow: '3px 3px 5px 1px #e3e3e3',
+  },
 	{
-		shadow: '0px 0px 5px #e3e3e3',
+		shadow: '0px 5px 8px 0px #f0f0f0, 0px 2px 5px 0px #f5f5f5'
 	},
-	{
-		shadow: '0px 0px 5px #d8d8d8',
+		{
+		shadow: 'inset 4px 4px 10px 0px #ececec, 4px 4px 10px 0px #ececec'
 	},
-	{
-		shadow: '0px 0px 5px #cccccc',
-	},
-	{
-		shadow: '0px 0px 5px #c1c1c1',
-	},
-	{
-		shadow: '0px 0px 5px #b6b6b6',
-	},
-	{
-		shadow: '0px 0px 5px #ababab',
-	},
-	{
-		shadow: '0px 0px 5px #a0a0a0',
-	},
-	{
-		shadow: '0px 0px 5px #949494',
-	},
-	{
-		shadow: '0px 0px 5px #898989',
-	},
-	{
-		shadow: '0px 0px 5px #7e7e7e',
-	},
-	{
-		shadow: '0px 0px 5px #737373',
-	},
-	{
-		shadow: '0px 0px 5px #676767',
-	},
-	{
-		shadow: '0px 0px 5px #5c5c5c',
-	}
+// ... add more here
 ];
