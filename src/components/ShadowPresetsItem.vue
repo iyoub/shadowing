1452521f3a63edfdbd4e18a1e4cf884b8ff99fe1
iyoub <abbous.ayoub@gmail.com>
2024-01-28 15:03:32 +0100

    added ability to click on preset to copy the code

--- src/components/ShadowPresetsItem.vue
+++ src/components/ShadowPresetsItem.vue
@@ -2,7 +2,14 @@
   <TabPane :tabs="tabs">
     <template #preview>
       <div class="card-preset-preview">
-        <div class="w-32 h-32 rounded-lg" :style="`box-shadow: ${props.preset.shadow};`"></div>
+        <div
+          class="w-32 h-32 rounded-lg flex items-center justify-center cursor-pointer transition duration-200 ease-in-out scale-100 hover:scale-95"
+          :style="`box-shadow: ${props.preset.shadow};`"
+          @click="copyCode"
+        >
+          <span class="text-[11px] text-neutral-400 text-center leading-tight" v-html="boxText">
+          </span>
+        </div>
       </div>
     </template>
     <template #code>
@@ -14,7 +21,7 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import HighlightCode from './HighlightCode.vue'
 import TabPane from './TabPane.vue'
 
@@ -37,13 +44,16 @@ const tabs = [
   }
 ]
 
+const boxText = ref<string>('Click to<br />Copy')
 const code = computed(() => {
   return `box-shadow: ${props.preset.shadow};`
 })
-</script>
 
-<style scoped>
-.card-preset-preview {
-  @apply bg-white rounded shadow p-6 min-h-60 flex items-center justify-center h-full;
+const copyCode = () => {
+  navigator.clipboard.writeText(code.value)
+  boxText.value = 'Copied ✅'
+  setTimeout(() => {
+    boxText.value = 'Click to<br />Copy'
+  }, 1000)
 }
-</style>
+</script>
